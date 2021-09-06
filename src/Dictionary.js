5d3238690aa7671a2cca3d143a6d697e992b0646
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-09-06 20:53:27 +0200

    imported API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    //documentation https://dictionaryapi.dev
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
