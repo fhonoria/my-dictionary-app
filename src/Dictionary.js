07055eb338d5beb3e39a11fa7e58b8c563e62b5e
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-09-06 21:15:14 +0200

    added Results component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     //documentation https://dictionaryapi.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
@@ -24,28 +25,33 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
+      <div className="Dictionary-header">
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-4">
+                <h1>Dictionary</h1>
+              </div>
+              <div className="col-4">
+                <input
+                  type="text"
+                  className="form-control shadow"
+                  onChange={handleKeyword}
+                ></input>
+              </div>
+              <div className="col-4">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary"
+                ></input>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
       <div className="container">
-        <form onSubmit={handleSubmit}>
-          <div className="row Header">
-            <div className="col-4">
-              <h1>Dictionary</h1>
-            </div>
-            <div className="col-4">
-              <input
-                type="text"
-                className="form-control shadow"
-                onChange={handleKeyword}
-              ></input>
-            </div>
-            <div className="col-4">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary"
-              ></input>
-            </div>
-          </div>
-        </form>
+        <Results results={results} />
       </div>
     </div>
   );
