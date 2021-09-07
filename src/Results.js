2670dbc5dc9b3a334d815b2ce21e824ed1c03083
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-09-07 20:53:33 +0200

    fixed error

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 export default function Results(props) {
-  console.log(props.results);
-  console.log(props.results.meanings[0].partOfSpeech);
-  return (
-    <div className="Results">
-      <h2>{props.results.word}</h2>
-      <h3>{props.results.meanings[0].partOfSpeech}</h3>
-    </div>
-  );
+  if (props.results) {
+    console.log(props.results);
+    console.log(props.results.meanings[0].partOfSpeech);
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        <h3>{props.results.meanings[0].partOfSpeech}</h3>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
